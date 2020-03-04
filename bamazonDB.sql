15279eb902fdb517bd8a8dbbfe09badbf0fd3d26
Jordan Servos <jservos11@yahoo.com>
2020-03-03 18:19:30 -0700

    installed packages, created product table, console.table

---
+++ bamazonDB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+item_id INT AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(50) NOT NULL,
+product_sales DECIMAL(10,2) NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INT(10) NOT NULL,
+PRIMARY KEY(item_id)
+
+);
+
